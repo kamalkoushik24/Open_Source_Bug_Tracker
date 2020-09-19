2b0815cb73dce7510cba25ae61c94c36c999b3f7
kamalkoushik <kamalkoushik24>
2020-09-20 04:15:28 +0530

    Must fix the issue of not able to restore spinner items.

--- app/src/main/java/com/example/itcbugtracker/LogActivity.java
+++ app/src/main/java/com/example/itcbugtracker/LogActivity.java
@@ -10,6 +10,8 @@ import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
 
@@ -17,22 +19,67 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.textfield.TextInputEditText;
 
 public class LogActivity extends AppCompatActivity {
+    TextInputEditText desc;
+    Spinner spinner1;
+    Spinner spinner2;
+    EditText pDetails;
+    int id;
+//    TODO
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log);
         final Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-        final int id = getIntent().getIntExtra("id", 0);
-        TextInputEditText desc;
-        Spinner spinner1;
-        Spinner spinner2;
+        id = getIntent().getIntExtra("id", 0);
+
         FloatingActionButton delbtn;
+        FloatingActionButton savebtn;
+        FloatingActionButton archivebtn;
         LogDatabase logDatabase;
 
 
 
+        desc = findViewById(R.id.desc);
+
         delbtn = findViewById(R.id.log_delete);
+        savebtn = findViewById(R.id.savebtn);
+        spinner1 = findViewById(R.id.spinner);
+        spinner2 = findViewById(R.id.spinner2);
+        pDetails = findViewById(R.id.pDetails);
+        archivebtn = findViewById(R.id.archive);
+        String desc1 = getIntent().getStringExtra("desc");
+        desc.setText(desc1);
+        String cat1 = getIntent().getStringExtra("category1");
+
+
+        ArrayAdapter<CharSequence> adapter1 = ArrayAdapter.createFromResource(this,
+                R.array.Application, android.R.layout.simple_spinner_item);
+        adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner1.setAdapter(adapter1);
+        if (spinner1.getSelectedItem().toString().equals("App1")){
+
+            ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(this,
+                    R.array.category1, android.R.layout.simple_spinner_item);
+            adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            spinner2.setAdapter(adapter2);
+
+        }
+        else if (spinner1.getSelectedItem().toString().equals("App2")){
+            ArrayAdapter<CharSequence> adapter3 = ArrayAdapter.createFromResource(this,
+                    R.array.category3, android.R.layout.simple_spinner_item);
+            adapter3.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            spinner2.setAdapter(adapter3);
+        }
+        else if (spinner1.getSelectedItem().toString().equals("App3")){
+            ArrayAdapter<CharSequence> adapter4 = ArrayAdapter.createFromResource(this,
+                    R.array.category3, android.R.layout.simple_spinner_item);
+            adapter4.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            spinner2.setAdapter(adapter4);
+        }
+
+
+
         delbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -43,9 +90,35 @@ public class LogActivity extends AppCompatActivity {
 
             }
         });
+
+        savebtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MainActivity.logDatabase.logDao().save(desc.getText().toString(),
+                        spinner1.getSelectedItem().toString(),
+                        spinner2.getSelectedItem().toString(),
+                        pDetails.getText().toString(), id);
+                startActivity(intent);
+            }
+        });
+        archivebtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                desc.setEnabled(false);
+                spinner1.setEnabled(false);
+                spinner2.setEnabled(false);
+                pDetails.setEnabled(false);
+            }
+        });
+
         }
 
-
-
-
+    @Override
+    protected void onPause() {
+        super.onPause();
+        MainActivity.logDatabase.logDao().save(desc.getText().toString(),
+                spinner1.getSelectedItem().toString(),
+                spinner2.getSelectedItem().toString(),
+                pDetails.getText().toString(), id);
     }
+}
